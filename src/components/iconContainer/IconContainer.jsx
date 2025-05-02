08dbf8972daba89911e981dd1ab906b1cca351ec
edmimi <mirisaor02@gmail.com>
2025-05-02 12:55:17 +0200

    FEAT-Icon Container created

---
+++ src/components/iconContainer/IconContainer.jsx
@@ -0,0 +1,28 @@
+import { FaHeart, FaHome, FaGoogle, FaApple, FaFacebook } from "react-icons/fa";
+import { IoHeartCircleOutline } from "react-icons/io5";
+import styles from './IconContainer.module.css';
+
+
+const icons = {
+ home: FaHome,
+ favorites: IoHeartCircleOutline,
+ google: FaGoogle,
+ apple: FaApple,
+ facebook: FaFacebook,
+ like: FaHeart
+};
+
+
+const IconContainer = ({ variation = "google", size = 48, onClick, className="" }) => {
+ const IconComponent = icons[variation] || FaBell;
+
+
+ return (
+   <button className={`${styles.iconContainer} ${styles[variation]} ${className}`} onClick={onClick}>
+     <IconComponent className={styles.iconImg} size={size} />
+   </button>
+ );
+};
+
+
+export default IconContainer;
