bdfe66ce7cf0c904c5ce4373434ddda6dcf2e435
marianamarinflor622 <marianamarinflor@gmail.com>
2025-05-05 10:16:05 +0200

    FEAT-add footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,18 @@
+import Footer from "../../components/footer/Footer";
 import Header from "../../components/Header/Header";
 import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <><Header />
-<>
-      <h1 className={styles.title}>Home</h1>
-    </></>
+    <div className={styles.pageContainer}>
+      <Header />
+      <main className={styles.mainContent}>
+        <h1 className={styles.title}>Home</h1>
+        
+      </main>
+      <Footer />
+    </div>
   );
 }
- 
-export default Home;
 
+export default Home;
