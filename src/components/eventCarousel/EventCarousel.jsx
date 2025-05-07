8fbd07eae1f89ceef6e2118ebbb4661d13948e43
marianamarinflor622 <marianamarinflor@gmail.com>
2025-05-07 12:31:47 +0200

    FEAT-add images al carousel

--- src/components/eventCarousel/EventCarousel.jsx
+++ src/components/eventCarousel/EventCarousel.jsx
@@ -4,6 +4,12 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from "../eventCarousel/EventCarousel.module.css";
 
+import foto1 from "../../assets/images /foto1.jpg";
+import foto2 from "../../assets/images /foto2.jpg";
+import foto3 from "../../assets/images /foto3.jpg";
+import foto4 from "../../assets/images /foto4.jpg";
+import foto5 from "../../assets/images /foto5.jpg";
+
 const EventCarousel = () => {
   const sliderRef = React.useRef(null);
 
@@ -11,34 +17,26 @@ const EventCarousel = () => {
     dots: false,
     infinite: true,
     speed: 400,
-    slidesToShow: 1, 
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
   };
 
-  const images = [
-    "https://plus.unsplash.com/premium_photo-1709579654090-3f3ca8f8416b?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJhbGV6YSUyMHBhaXNhamV8ZW58MHx8MHx8fDA%3D",
-    "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?cs=srgb&dl=pexels-souvenirpixels-417074.jpg&fm=jpg",
-    "https://www.dzoom.org.es/wp-content/uploads/2022/03/mis-fotografias-paisaje-favoritas-07-810x540.jpg",
-    "https://mott.pe/noticias/wp-content/uploads/2019/03/los-50-paisajes-maravillosos-del-mundo-para-tomar-fotos.jpg",
-    "https://cdn.shortpixel.ai/spai/q_lossless+w_1082+to_auto+ret_img/independent-photo.com/wp-content/uploads/2022/02/Yifeng-Ding-scaled.jpeg",
-  ];
+  const images = [foto1, foto2, foto3, foto4, foto5];
 
   return (
-    
     <div className={styles.carouselContainer}>
       <Slider ref={sliderRef} {...settings}>
         {images.map((src, index) => (
           <div key={index} className={styles.slide}>
-            <img src={src} alt={`Slide ${index + 1}`} className={styles.image} />
+            <img src={src} alt={`Foto ${index + 1}`} className={styles.image} />
           </div>
         ))}
       </Slider>
       <button
         className={`${styles.navButton} ${styles.prevButton}`}
         onClick={() => sliderRef.current.slickPrev()}
-
       >
         {"<"}
       </button>
